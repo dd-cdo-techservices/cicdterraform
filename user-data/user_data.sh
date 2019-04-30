33e11a197aaa1c12049d468ea1e0564806db3a56
sumanthroyale <sumanthroyale@users.noreply.github.com>
2019-04-30 16:19:07 +0530

    removing user data

--- user-data/user_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo useradd ansibleuser
-echo "TestMe@10" | passwd --stdin ansibleuser
-sudo sed -i 's/#PasswordAuthentication/PasswordAuthentication/' /etc/ssh/sshd_config
-sudo service sshd restart
